07fbc56e7c977452b131630d2e806af4ed062494
hajarNasr <hajar@weventure.global>
2021-08-26 09:23:23 +0200

    refactor: add Schedules to Home

--- src/containers/home/index.js
+++ src/containers/home/index.js
@@ -1,9 +1,11 @@
 import "./styles.scss";
 import Dashboard from "../../components/dashboard";
+import Schedules from "../../components/schedules";
 
 const Home = () => (
   <main id="home">
     <Dashboard />
+    <Schedules />
   </main>
 );
 export default Home;
