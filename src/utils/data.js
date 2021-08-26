eef4a65270d1cb96e7476f13a91371d25f19bbdd
hajarNasr <hajar@weventure.global>
2021-08-26 09:20:44 +0200

    feat: add mockup data for charts

--- src/utils/data.js
+++ src/utils/data.js
@@ -13,3 +13,83 @@ export const teamMembersData = [
   { name: "Sanra B.", avatar: FACE_2, title: "Web Developer" },
   { name: "Nabil Ibrahim", avatar: FACE_1, title: "Android Developer" },
 ];
+
+export const chartData = [
+  {
+    name: "Jan",
+    uv: 4000,
+    pv: 2400,
+    sd: 1000,
+    amt: 2400,
+  },
+  {
+    name: "Feb",
+    uv: 3000,
+    pv: 1398,
+    sd: 1200,
+    amt: 2210,
+  },
+  {
+    name: "Mar",
+    uv: 2000,
+    pv: 9800,
+    sd: 2000,
+    amt: 2290,
+  },
+  {
+    name: "Apr",
+    uv: 2780,
+    pv: 3908,
+    sd: 2100,
+    amt: 2000,
+  },
+  {
+    name: "Jun",
+    uv: 1890,
+    pv: 4800,
+    sd: 1500,
+    amt: 2181,
+  },
+  {
+    name: "Jul",
+    uv: 2390,
+    pv: 3800,
+    sd: 1700,
+    amt: 2500,
+  },
+  {
+    name: "Aug",
+    uv: 3490,
+    pv: 4300,
+    sd: 1800,
+    amt: 2100,
+  },
+  {
+    name: "Sep",
+    uv: 3490,
+    pv: 4300,
+    sd: 2000,
+    amt: 2100,
+  },
+  {
+    name: "Oct",
+    uv: 3490,
+    pv: 4300,
+    sd: 1200,
+    amt: 2100,
+  },
+  {
+    name: "nov",
+    uv: 3490,
+    pv: 4300,
+    sd: 1500,
+    amt: 2100,
+  },
+  {
+    name: "Dec",
+    uv: 3490,
+    pv: 4300,
+    sd: 2000,
+    amt: 2100,
+  },
+];
